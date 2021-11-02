7a4956a9863ea787289cf02f31db1980dd3b2320
Adrian <57720717+Vxschlxfner@users.noreply.github.com>
2021-11-02 10:09:07 +0100

    it should work now

--- src/TransferAPI/api/TransferAPI.php
+++ src/TransferAPI/api/TransferAPI.php
@@ -20,26 +20,20 @@ class TransferAPI {
      * Function transferPlayer
      * @param Player $player
      * @param string $servername
-     * @param int $port
      * @return void
      */
-    public static function transferPlayer(Player $player, string $servername, int $port=0){
+    public static function transferPlayer(Player $player, string $servername){
 
         if (is_null($servername) or !is_string($servername)){
             Server::getInstance()->getLogger()->alert("§4Please insert an valid Server name.");
             return;
         }
 
-        if (is_null($port) or !is_numeric($port)){
-            Server::getInstance()->getLogger()->alert("§4Please insert an valid Port.");
-            return;
-        }
-
         $pk = new TransferPacket();
         $pk->address = $servername; //The server name you specified in the WaterDogPE config.
-        $pk->port = $port; //The server port you specified in the WaterDogPE config.
+        $pk->port = 0; //The server port you specified in the WaterDogPE config.
         $player->sendDataPacket($pk);
-        Server::getInstance()->getLogger()->info($player->getName() . "§c will teleported to Server §e{$servername} §c with Port §e{$port}§8.");
+        Server::getInstance()->getLogger()->info($player->getName() . "§c will teleported to Server §e{$servername}§8.");
     }
 
 }
